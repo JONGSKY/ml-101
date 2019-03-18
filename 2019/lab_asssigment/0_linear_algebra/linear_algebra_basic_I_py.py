064f922e122bb0a530defab85e2a50dda7cf189b
JONGSKY <ljhljh0125@gmail.com>
2019-03-18 23:47:38 +0900

    lab_0 -ing

--- 2019/lab_asssigment/0_linear_algebra/linear_algebra_basic_I_py.py
+++ 2019/lab_asssigment/0_linear_algebra/linear_algebra_basic_I_py.py
@@ -1,50 +1,66 @@
 def vector_size_check(*vector_variables):
-    return None
+    result = len(set([len(vector) for vector in vector_variables])) == 1
+    return result
+
 
 def vector_addition(*vector_variables):
-    return None
+    result = [sum(vector) for vector in zip(*vector_variables)]
+    return result
+
 
 
 def vector_subtraction(*vector_variables):
     if vector_size_check(*vector_variables) == False:
         raise ArithmeticError
-    return None
+    else:
+        result = [2*vector[0]-sum(vector) for vector in zip(*vector_variables)]
+    return result
 
 
 def scalar_vector_product(alpha, vector_variable):
-    return None
+    result = [alpha*vector for vector in vector_variable]
+    return result
 
 
 def matrix_size_check(*matrix_variables):
-    return None
+    result = len(set([len(a) for a in (matrix_variables)]))==1 and len(set([len(matrix[0]) for matrix in matrix_variables]))==1
+    return result
 
 
 def is_matrix_equal(*matrix_variables):
-    return None
+    result = all([ all([len(set(k))==1 for k in zip(*i)]) for i in zip(*matrix_variables)])
+    return result
 
 
 def matrix_addition(*matrix_variables):
     if matrix_size_check(*matrix_variables) == False:
         raise ArithmeticError
-    return None
+    else:
+        result = [[sum(row) for row in zip(*t)] for t in zip(*matrix_variables)]
+    return result
 
 
 def matrix_subtraction(*matrix_variables):
     if matrix_size_check(*matrix_variables) == False:
         raise ArithmeticError
-    return None
+    else:
+        result = [[2*k[0]-sum(k) for k in zip(*i)]for i in zip(*matrix_variables)]
+    return result
 
 
 def matrix_transpose(matrix_variable):
-    return None
+    result = [[k for k in i] for i in zip(*matrix_variable)]
+    return result
 
 
 def scalar_matrix_product(alpha, matrix_variable):
-    return None
+    result = [[ alpha*k for k in i]for i in matrix_variable]
+    return result
 
 
 def is_product_availability_matrix(matrix_a, matrix_b):
-    return None
+    result = len(matrix_a[0])==len(matrix_b)
+    return result
 
 
 def matrix_product(matrix_a, matrix_b):
